9f0d41831b3c1c38b67ace596280ecacd122154f
sean-garwood <seansgarwood@gmail.com>
2026-01-20 17:49:21 -0500

    feat: make 6502 do something
    Hardcoded a rom full of mostly no ops. But also:
    
    * set reset vector value to point to 0x8000, which contains a couple
      instructions and args
    
    ```asm
    LDA 0x42 ; add 0x42 (random value) to the A register
    STA 0x6000 ; STA absolute is 0x8D, which stores the contents of
                 the A register to the sole arg.
    ```

--- src/makerom.py
+++ src/makerom.py
@@ -1,9 +1,18 @@
-# fill EEPROM with nop
-
-NOP_OPCODE = 0xEA
 SIZE = 0x8000 # EEPROM capacity = 32KiB
+OUTFILE = "/home/ssg/courses/6502/bin/rom.bin"
+LDA = 0xA9  # LDA Immediate opcode
+STA = 0x8D  # STA Absolute opcode
 
-rom = bytearray([NOP_OPCODE] * SIZE)
+rom = bytearray([0xEA] * SIZE)
 
-with open("../bin/rom.bin", "wb") as f:
+rom[0] = LDA
+rom[1] = 0x42
+rom[2] = STA
+rom[3] = 0x00
+rom[4] = 0x60  # Store value 0x42 at address 0x6000
+
+rom[0x7FFC] = 0x00  # Reset vector low byte
+rom[0x7FFD] = 0x80  # Reset vector high byte
+
+with open(OUTFILE, "wb") as f:
     f.write(rom)
