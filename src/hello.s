7686e4df987ac9b9a230f6c9940a6e75516d4c4b
sean-garwood <seansgarwood@gmail.com>
2026-01-23 10:26:47 -0500

    feat(src): hello.s
    Attempt to write an H to the LCD.

---
+++ src/hello.s
@@ -0,0 +1,60 @@
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003
+
+E  = %10000000
+RW = %01000000
+RS = %00100000
+
+    .org $8000     ; entrypoint
+
+reset:
+    lda #%11111111 ; Set all pins on port B to output
+    sta DDRB
+
+    lda #%11100000 ; Set top 3 pins (D5-D7) of port A to output
+    sta DDRA
+
+    lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
+    sta PORTB
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+    lda #E         ; Set Enable pin to send instruction
+    sta PORTA
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+
+    lda #%00001110 ; Display/cursor on; blink off
+    sta PORTB
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+    lda #E         ; Set Enable pin to send instruction
+    sta PORTA
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+
+    lda #%00000110 ; Increment and shift cursor; don't shift display
+    sta PORTB
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+    lda #E         ; Set Enable pin to send instruction
+    sta PORTA
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+
+    lda #"H"
+    sta PORTB
+    lda #RS        ; Set RS; Clear RW/E bits
+    sta PORTA
+    lda #(RS | E)
+    sta PORTA
+    lda #RS        ; Clear E bits
+    sta PORTA
+
+loop:
+    jmp loop
+
+    .org $fffc
+    .word reset
+    .word $0000
